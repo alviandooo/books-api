547fee68645742b57775cd66c5af894d25b02385
Alviando <restualviando28@gmail.com>
2025-02-09 14:23:37 +0700

    [feat] API Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
@@ -11,4 +12,9 @@ class Category extends Model
         'name',
         'created_by',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by', 'id');
+    }
 }
